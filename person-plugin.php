2d13f8bbd18460e9ba751bc8c51a65f435ac34f3
felip-araujo <felipearaujoexecutive@gmail.com>
2025-09-12 13:21:44 -0400

    3.2.3

--- person-plugin.php
+++ person-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Person Plugin - Editor de Adesivos
 Description: Plugin para edição de (Arquivos SVG) edite e gerencie seus arquivos de forma prática.
-Version: 3.2.2 - Envio de SVG e PNG para o ADMIN
+Version: 3.2.3 - Envio de SVG e PNG para o ADMIN
 Author: Evolution Design
 Author URI:  https://evoludesign.com.br/
 */
